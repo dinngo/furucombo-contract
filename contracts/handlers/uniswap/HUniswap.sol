e94dc35481a9c79abc54439ce7d6365362d37d16
hihiben <hihiben@gmail.com>
2020-03-18 19:21:39 +0800

    Refine swap test

--- contracts/handlers/uniswap/HUniswap.sol
+++ contracts/handlers/uniswap/HUniswap.sol
@@ -28,6 +28,7 @@ contract HUniswap is HandlerBase {
         _updateToken(token);
     }
 
+    // TODO: test
     function ethToTokenSwapOutput(
         uint256 value,
         address token,
@@ -35,9 +36,24 @@ contract HUniswap is HandlerBase {
     ) external payable returns (uint256 eth_sold) {
         IUniswapFactory uniswapFactory = IUniswapFactory(getFactory());
         IUniswapExchange uniswap = IUniswapExchange(uniswapFactory.getExchange(token));
-        uniswap.ethToTokenSwapOutput.value(value)(tokens_bought, now);
+        eth_sold = uniswap.ethToTokenSwapOutput.value(value)(tokens_bought, now);
 
         // Update involved token
         _updateToken(token);
+    }
+
+    function addLiquidity(
+        uint256 value,
+        address token,
+        uint256 max_tokens
+    ) external payable returns (uint256 liquidity) {
+        IUniswapFactory uniswapFactory = IUniswapFactory(getFactory());
+        IUniswapExchange uniswap = IUniswapExchange(uniswapFactory.getExchange(token));
+        IERC20(token).safeApprove(address(uniswap), max_tokens);
+        liquidity = uniswap.addLiquidity.value(value)(1, max_tokens, now + 1);
+        IERC20(token).safeApprove(address(uniswap), 0);
+
+        // Update involved token
+        _updateToken(address(uniswap));
     }
 }
