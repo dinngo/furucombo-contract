0f818fee3d6538ad7b4b4bf994245a5020a007af
benhuang <ben@dinngo.co>
2020-04-22 12:00:04 +0800

    Redeem functions

--- contracts/handlers/compound/HCEther.sol
+++ contracts/handlers/compound/HCEther.sol
@@ -1,9 +1,13 @@
 pragma solidity ^0.5.0;
 
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
 import "../HandlerBase.sol";
 import "./ICEther.sol";
 
 contract HCEther is HandlerBase {
+    using SafeERC20 for IERC20;
+
     address constant CETHER = 0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5;
 
     function mint(uint256 value) external payable {
@@ -12,5 +16,19 @@ contract HCEther is HandlerBase {
 
         // Update involved token
         _updateToken(CETHER);
+    }
+
+    function redeem(uint256 redeemTokens) external payable {
+        ICEther compound = ICEther(CETHER);
+        IERC20(CETHER).safeApprove(CETHER, redeemTokens);
+        compound.redeem(redeemTokens);
+        IERC20(CETHER).safeApprove(CETHER, 0);
+    }
+
+    function redeemUnderlying(uint256 redeemAmount) external payable {
+        ICEther compound = ICEther(CETHER);
+        IERC20(CETHER).safeApprove(CETHER, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF);
+        compound.redeemUnderlying(redeemAmount);
+        IERC20(CETHER).safeApprove(CETHER, 0);
     }
 }
