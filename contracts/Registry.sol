9970617b5c50a92f57e7213843be7c85b551dce7
benhuang <ben@dinngo.co>
2020-05-29 15:05:19 +0800

    Comments

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -3,17 +3,30 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 
+/// @notice The handler registry database for Furucombo
 contract Registry is Ownable {
     mapping(address => bytes32) handlers;
 
     bytes32 constant DEPRECATED = bytes10(0x64657072656361746564);
 
+    /**
+     * @notice Register a handler with a bytes32 information.
+     * @param registration Handler address.
+     * @param info Info string.
+     * @dev Dapps that triggers callback function should also be registered.
+     * In this case, registration is the Dapp address and the leading 20 bytes
+     * is the handler address.
+     */
     function register(address registration, bytes32 info) external onlyOwner {
         require(registration != address(0), "zero address");
         require(handlers[registration] == bytes32(0), "registered");
         handlers[registration] = info;
     }
 
+    /**
+     * @notice Unregister a handler. The handler will be deprecated.
+     * @param registration The handler to be unregistered.
+     */
     function unregister(address registration) external onlyOwner {
         require(registration != address(0), "zero address");
         require(handlers[registration] != bytes32(0), "no registration");
@@ -21,12 +34,20 @@ contract Registry is Ownable {
         handlers[registration] = DEPRECATED;
     }
 
+    /**
+     * @notice Check if the handler is valid.
+     * @param handler The handler to be verified.
+     */
     function isValid(address handler) external view returns (bool result) {
         if (handlers[handler] == 0 || handlers[handler] == DEPRECATED)
             return false;
         else return true;
     }
 
+    /**
+     * @notice Get the information of a registration.
+     * @param handler The handler address to be queried.
+     */
     function getInfo(address handler) external view returns (bytes32 info) {
         return handlers[handler];
     }
