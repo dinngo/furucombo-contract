e2e0d5e99bfdf7d95ab438a0ad86d74a9389e642
Albert Lin <albert@dinngo.co>
2020-06-10 12:58:30 +0800

    Add uniswapv2 add liquidity token handler test case

--- test/HUniswapV2_liquidity.test.js
+++ test/HUniswapV2_liquidity.test.js
@@ -14,9 +14,12 @@ const utils = web3.utils;
 const { expect } = require('chai');
 const {
   DAI_TOKEN,
+  BAT_TOKEN,
   DAI_PROVIDER,
+  BAT_PROVIDER,
   ETH_PROVIDER,
   UNISWAPV2_ETH_DAI,
+  UNISWAPV2_BAT_DAI,
 } = require('./utils/constants');
 const { resetAccount, profileGas } = require('./utils/utils');
 
@@ -26,9 +29,16 @@ const Proxy = artifacts.require('ProxyMock');
 const IToken = artifacts.require('IERC20');
 
 contract('UniswapV2 Liquidity', function([_, deployer, user]) {
-  const tokenAddress = DAI_TOKEN;
-  const providerAddress = DAI_PROVIDER;
+  // const tokenAddress = DAI_TOKEN;
+  // const providerAddress = DAI_PROVIDER;
+  // const uniswapV2ETHDAIAddress = UNISWAPV2_ETH_DAI;
+
+  const tokenAAddress = DAI_TOKEN;
+  const tokenBAddress = BAT_TOKEN;
+  const tokenAProviderAddress = DAI_PROVIDER;
+  const tokenBProviderAddress = BAT_PROVIDER;
   const uniswapV2ETHDAIAddress = UNISWAPV2_ETH_DAI;
+  const uniswapV2BATDAIAddress = UNISWAPV2_BAT_DAI;
 
   let balanceUser;
   let tokenUser;
@@ -42,25 +52,35 @@ contract('UniswapV2 Liquidity', function([_, deployer, user]) {
       this.huniswapv2.address,
       utils.asciiToHex('UniswapV2')
     );
-    this.token = await IToken.at(tokenAddress);
-    this.uniToken = await IToken.at(uniswapV2ETHDAIAddress);
-    await this.token.transfer(user, ether('1000'), { from: providerAddress });
+    this.tokenA = await IToken.at(tokenAAddress);
+    this.tokenB = await IToken.at(tokenBAddress);
+    this.uniTokenETH = await IToken.at(uniswapV2ETHDAIAddress);
+    this.uniTokenToken = await IToken.at(uniswapV2BATDAIAddress);
+
+    await this.tokenA.transfer(user, ether('1000'), {
+      from: tokenAProviderAddress,
+    });
+    await this.tokenB.transfer(user, ether('1000'), {
+      from: tokenBProviderAddress,
+    });
   });
 
   beforeEach(async function() {
     await resetAccount(_);
     await resetAccount(user);
     balanceUser = await tracker(user);
-    tokenUser = await this.token.balanceOf.call(user);
-    uniTokenUser = await this.uniToken.balanceOf.call(user);
+    tokenAUser = await this.tokenA.balanceOf.call(user);
+    tokenBUser = await this.tokenB.balanceOf.call(user);
+    uniTokenETHUser = await this.uniTokenETH.balanceOf.call(user);
+    uniTokenTokenUser = await this.uniTokenToken.balanceOf.call(user);
   });
 
   describe('Add ETH', function() {
     beforeEach(async function() {
-      await this.token.transfer(this.proxy.address, ether('100'), {
+      await this.tokenA.transfer(this.proxy.address, ether('100'), {
         from: user,
       });
-      await this.proxy.updateTokenMock(this.token.address);
+      await this.proxy.updateTokenMock(this.tokenA.address);
     });
 
     it('normal', async function() {
@@ -72,7 +92,7 @@ contract('UniswapV2 Liquidity', function([_, deployer, user]) {
       const data = abi.simpleEncode(
         'addLiquidityETH(uint256,address,uint256,uint256,uint256)',
         value,
-        tokenAddress,
+        tokenAAddress,
         daiAmount,
         minDaiAmount,
         minEthAmount
@@ -91,13 +111,62 @@ contract('UniswapV2 Liquidity', function([_, deployer, user]) {
       );
 
       // Check spent token
-      expect(await this.token.balanceOf.call(user)).to.be.bignumber.lte(
-        tokenUser.sub(minDaiAmount)
+      expect(await this.tokenA.balanceOf.call(user)).to.be.bignumber.lte(
+        tokenAUser.sub(minDaiAmount)
       );
 
       // TODO: Find out the exact number of uniToken for testing
-      expect(await this.uniToken.balanceOf.call(user)).to.be.bignumber.gt(
-        uniTokenUser
+      expect(await this.uniTokenETH.balanceOf.call(user)).to.be.bignumber.gt(
+        uniTokenETHUser
+      );
+      profileGas(receipt);
+    });
+  });
+
+  describe('Add Token', function() {
+    beforeEach(async function() {
+      await this.tokenA.transfer(this.proxy.address, ether('100'), {
+        from: user,
+      });
+      await this.tokenB.transfer(this.proxy.address, ether('100'), {
+        from: user,
+      });
+      await this.proxy.updateTokenMock(this.tokenA.address);
+      await this.proxy.updateTokenMock(this.tokenB.address);
+    });
+
+    it('normal', async function() {
+      const tokenAAmount = ether('0.002');
+      const tokenBAmount = ether('0.002');
+      const minTokenAAmount = ether('0.000001');
+      const minTokenBAmount = ether('0.000001');
+      const to = this.huniswapv2.address;
+      const data = abi.simpleEncode(
+        'addLiquidity(address,address,uint256,uint256,uint256,uint256)',
+        tokenAAddress,
+        tokenBAddress,
+        tokenAAmount,
+        tokenBAmount,
+        minTokenAAmount,
+        minTokenBAmount
+      );
+
+      const receipt = await this.proxy.execMock(to, data, {
+        from: user,
+      });
+
+      // Check spent token
+      expect(await this.tokenA.balanceOf.call(user)).to.be.bignumber.lte(
+        tokenAUser.sub(minTokenAAmount)
+      );
+
+      expect(await this.tokenB.balanceOf.call(user)).to.be.bignumber.lte(
+        tokenBUser.sub(minTokenBAmount)
+      );
+
+      // TODO: Find out the exact number of uniToken for testing
+      expect(await this.uniTokenToken.balanceOf.call(user)).to.be.bignumber.gt(
+        uniTokenTokenUser
       );
       profileGas(receipt);
     });
