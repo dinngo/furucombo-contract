0bf6a51e073913936f232642813380f257ee11d1
benhuang <ben@dinngo.co>
2020-06-11 17:56:21 +0800

    Fix ProxyMock for testing

--- contracts/mocks/ProxyMock.sol
+++ contracts/mocks/ProxyMock.sol
@@ -13,13 +13,26 @@ contract ProxyMock is Proxy, GasProfiler {
         payable
         returns (bytes memory result)
     {
-        cache.setSender(msg.sender);
+        _preProcess();
         _setBase();
         result = _exec(to, data);
         _setPostProcess(to);
         _deltaGas("Gas");
         _postProcess();
         return result;
+    }
+
+    function _preProcess() internal {
+        // Set the sender on the top of cache.
+        if (cache.length != 0) {
+            cache.set(cache.peek());
+            for (uint256 i = cache.length - 1; i > 0; i--) {
+                cache[i] = cache[i - 1];
+            }
+            cache[0] = bytes32(uint256(uint160(msg.sender)));
+        } else {
+            cache.setSender(msg.sender);
+        }
     }
 
     function updateTokenMock(address token) public {
