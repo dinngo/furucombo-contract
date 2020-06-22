78ae7c4ab1fc23036da68a1be053407c97850d8a
benhuang <ben@dinngo.co>
2020-06-22 18:47:05 +0800

    Remove COMP address

--- contracts/handlers/compound/HComptroller.sol
+++ contracts/handlers/compound/HComptroller.sol
@@ -6,7 +6,6 @@ import "./IComptroller.sol";
 
 contract HComptroller is HandlerBase {
     address constant COMPTROLLER = 0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B;
-    address constant COMP_TOKEN = 0xc00e94Cb662C3520282E6f5717214004A7f26888;
 
     function claimComp() external payable {
         IComptroller comptroller = IComptroller(COMPTROLLER);
