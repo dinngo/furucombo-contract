cfe3aaed583e8b11426dae125d0cb224c86028d6
Albert Lin <albert@dinngo.co>
2020-08-18 14:18:57 +0800

    style(handler): update mooniswap deposit function parameter

--- contracts/handlers/mooniswap/HMooniswap.sol
+++ contracts/handlers/mooniswap/HMooniswap.sol
@@ -13,13 +13,12 @@ contract HMooniswap is HandlerBase {
     address payable public constant MooniFactory = 0x71CD6666064C3A1354a3B4dca5fA1E2D3ee7D303;
 
     function deposit(
-        address[] calldata tokens,
+        address[2] calldata tokens,
         uint256[] calldata amounts,
         uint256[] calldata minAmounts
     ) external payable returns (uint256 fairSupply) {
-        require(tokens.length == 2, "wrong tokens length");
-        require(amounts.length == 2, "wrong amounts length");
-        require(minAmounts.length == 2, "wrong min amounts length");
+        require(tokens[0] != tokens[1], "tokens can not be the same");
+        require(amounts.length == tokens.length, "wrong amounts length");
 
         IMooniFactory factory = IMooniFactory(MooniFactory);
         IMooniswap mooniswap = IMooniswap(factory.pools(tokens[0], tokens[1]));
