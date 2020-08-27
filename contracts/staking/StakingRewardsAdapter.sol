3c869347a62d3f9a2a46890f9c363ec7cae03026
Robin Pan <robin@dinngo.co>
2020-08-27 14:46:09 +0800

    Init commit

---
+++ contracts/staking/StakingRewardsAdapter.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.5.16;
+
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+
+import "./IStakingRewards.sol";
+
+contract StakingRewardsAdapter is ReentrancyGuard, Pausable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    IERC20 public rewardsToken;
+    IERC20 public stakingToken;
+    // uint256 public periodFinish = 0;
+    // uint256 public rewardRate = 0;
+    // uint256 public rewardsDuration = 7 days;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+}
