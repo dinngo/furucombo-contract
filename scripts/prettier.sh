be760100ccca16eadc0edb363438b85d519fab8d
benhuang <ben@dinngo.co>
2020-09-10 11:28:04 +0800

    Update prettier version

--- scripts/prettier.sh
+++ scripts/prettier.sh
@@ -6,7 +6,7 @@ FILES=$(git diff --cached --name-only --diff-filter=ACMR "*.js" "*.sol" | sed 's
 [ -z "$FILES" ] && exit 0
 
 # Prettify all selected files
-echo "$FILES" | xargs ./node_modules/prettier/bin-prettier.js --write
+echo "$FILES" | xargs ./node_modules/.bin/prettier --write
 
 # Add back the modified/prettified files to staging
 echo "$FILES" | xargs git add
