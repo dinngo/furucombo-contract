97bc7b3f9312fa256ab62b55223f1e11d1b9ed10
Albert Lin <albert@dinngo.co>
2020-12-07 15:55:20 +0800

    feat(funds): add checkSlippage func

--- contracts/handlers/funds/HFunds.sol
+++ contracts/handlers/funds/HFunds.sol
@@ -42,6 +42,30 @@ contract HFunds is HandlerBase {
         receiver.transfer(amount);
     }
 
+    function checkSlippage(
+        address[] calldata tokens,
+        uint256[] calldata amounts
+    ) external payable {
+        if (tokens.length != amounts.length) {
+            _revertMsg("checkSlippage", "token and amount does not match");
+        }
+
+        // FIXME: the detailed error message, ex. which token slippage over the limitation
+        // 討論一下有必要為了 error msg 就多寫一個 contact function 和 address to string function 增加風險嗎？
+
+        for (uint256 i = 0; i < tokens.length; i++) {
+            if (tokens[i] == address(0)) {
+                if (address(this).balance < amounts[i]) {
+                    _revertMsg("checkSlippage", "ETH slippage");
+                }
+                continue;
+            }
+            if (IERC20(tokens[i]).balanceOf(address(this)) < amounts[i]) {
+                _revertMsg("checkSlippage", "token slippage");
+            }
+        }
+    }
+
     function getBalance(address token) external payable returns (uint256) {
         if (token != address(0)) {
             return IERC20(token).balanceOf(address(this));
