e9fda09110b3b712a906183ddb589f66f7e1d008
benhuang <ben@dinngo.co>
2020-12-10 19:03:54 +0800

    Fix IComptroller

--- contracts/handlers/compound/IComptroller.sol
+++ contracts/handlers/compound/IComptroller.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.6.0;
 
-abstract contract IComptroller {
+interface IComptroller {
     function enterMarkets  (address[] calldata cTokens) external virtual returns (uint[] memory);
     function exitMarket(address cToken) external virtual returns (uint);
     function checkMembership(address account, address cToken) external view virtual returns (bool);
