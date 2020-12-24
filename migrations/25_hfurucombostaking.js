24c6e68348e33f10219d77cc8b99217aa711a6b3
Albert Lin <albert@dinngo.co>
2020-12-24 16:56:07 +0800

    ci(test): skip to deploy contract before running CI test cases

--- migrations/25_hfurucombostaking.js
+++ migrations/25_hfurucombostaking.js
@@ -3,6 +3,9 @@ const Handler = artifacts.require('HFurucomboStaking');
 const utils = web3.utils;
 
 module.exports = async function(deployer) {
+  if (deployer.network === 'development') {
+    return;
+  }
   await deployer.deploy(Handler);
   const registry = await Registry.deployed();
   await registry.register(
--- migrations/2_registry.js
+++ migrations/2_registry.js
@@ -1,5 +1,8 @@
 const Registry = artifacts.require('Registry');
 
 module.exports = function(deployer) {
+  if (deployer.network === 'development') {
+    return;
+  }
   deployer.deploy(Registry);
 };
