9b5eb9179c202d585ebb03e8d8f2a1748ef7bf4d
benhuang <ben@dinngo.co>
2020-12-29 17:48:26 +0800

    Add 1inch v2

---
+++ contracts/handlers/oneinchV2/HOneInchExchange.sol
@@ -0,0 +1,85 @@
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+import "../HandlerBase.sol";
+import "./IOneInchCaller.sol";
+import "./IOneInchExchangeV2.sol";
+
+contract HOneInchExchange is HandlerBase {
+    using SafeERC20 for IERC20;
+
+    // prettier-ignore
+    address public constant ONEINCH_SPENDER = 0x111111125434b319222CdBf8C261674aDB56F3ae;
+    address public constant ETH_ADDRESS =
+        0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    function getContractName() public pure override returns (string memory) {
+        return "HOneInchV2";
+    }
+
+    function discountedSwap(uint256 value, bytes calldata data)
+        external
+        payable
+        returns (uint256 returnAmount)
+    {
+        (, , IOneInchExchangeV2.SwapDescription memory desc, ) =
+            abi.decode(
+                data,
+                (
+                    bytes4,
+                    IOneInchCaller,
+                    IOneInchExchangeV2.SwapDescription,
+                    IOneInchCaller.CallDescription[]
+                )
+            );
+
+        if (address(desc.srcToken) != ETH_ADDRESS)
+            desc.srcToken.safeApprove(ONEINCH_SPENDER, desc.amount);
+        (bool success, bytes memory ret) =
+            ONEINCH_SPENDER.call{value: value}(data);
+        if (success) {
+            (returnAmount) = abi.decode(ret, (uint256));
+        } else {
+            _revertMsg("discountedSwap", string(ret));
+        }
+        if (address(desc.srcToken) != ETH_ADDRESS)
+            desc.srcToken.safeApprove(ONEINCH_SPENDER, 0);
+
+        // Update involved token
+        if (address(desc.dstToken) != ETH_ADDRESS)
+            _updateToken(address(desc.dstToken));
+    }
+
+    function swap(
+        uint256 value,
+        IERC20 srcToken,
+        IERC20 dstToken,
+        uint256 amount,
+        bytes memory data
+    ) public payable returns (uint256 returnAmount) {
+        if (address(srcToken) != ETH_ADDRESS) {
+            srcToken.safeApprove(ONEINCH_SPENDER, amount);
+            (bool success, bytes memory ret) =
+                ONEINCH_SPENDER.call{value: value}(data);
+            if (success) {
+                (returnAmount) = abi.decode(ret, (uint256));
+            } else {
+                _revertMsg("swap", string(ret));
+            }
+            srcToken.safeApprove(ONEINCH_SPENDER, 0);
+        } else {
+            (bool success, bytes memory ret) =
+                ONEINCH_SPENDER.call{value: value}(data);
+            if (success) {
+                (returnAmount) = abi.decode(ret, (uint256));
+            } else {
+                _revertMsg("swap", string(ret));
+            }
+        }
+
+        // Update involved token
+        if (address(dstToken) != ETH_ADDRESS) _updateToken(address(dstToken));
+    }
+}
---
+++ test/HOneInchV2.test.js
@@ -0,0 +1,374 @@
+const {
+  balance,
+  BN,
+  constants,
+  ether,
+  expectEvent,
+  expectRevert,
+  time,
+} = require('@openzeppelin/test-helpers');
+const { tracker } = balance;
+const { MAX_UINT256 } = constants;
+const { latest } = time;
+const abi = require('ethereumjs-abi');
+const util = require('ethereumjs-util');
+const utils = web3.utils;
+const { expect } = require('chai');
+const {
+  DAI_TOKEN,
+  DAI_PROVIDER,
+  DAI_SYMBOL,
+  USDC_TOKEN,
+  USDC_SYMBOL,
+  ZRX_TOKEN,
+  ZRX_SYMBOL,
+  KNC_TOKEN,
+  KNC_SYMBOL,
+} = require('./utils/constants');
+const {
+  evmRevert,
+  evmSnapshot,
+  mulPercent,
+  profileGas,
+  getHandlerReturn,
+} = require('./utils/utils');
+const fetch = require('node-fetch');
+const queryString = require('query-string');
+
+const HOneInchExchange = artifacts.require('HOneInchExchange');
+const Registry = artifacts.require('Registry');
+const Proxy = artifacts.require('ProxyMock');
+const IToken = artifacts.require('IERC20');
+
+contract('OneInch Swap', function([_, user]) {
+  let id;
+
+  before(async function() {
+    this.registry = await Registry.new();
+    this.hOneInchV2 = await HOneInchExchange.new();
+    await this.registry.register(
+      this.hOneInchV2.address,
+      utils.asciiToHex('OneInch V2')
+    );
+    this.proxy = await Proxy.new(this.registry.address);
+  });
+
+  beforeEach(async function() {
+    id = await evmSnapshot();
+  });
+
+  afterEach(async function() {
+    await evmRevert(id);
+  });
+
+  describe('Ether to Token', function() {
+    const tokenAddress = DAI_TOKEN;
+    const tokenSymbol = DAI_SYMBOL;
+
+    let balanceUser;
+    let balanceProxy;
+    let tokenUser;
+
+    before(async function() {
+      this.token = await IToken.at(tokenAddress);
+    });
+
+    beforeEach(async function() {
+      balanceUser = await tracker(user);
+      balanceProxy = await tracker(this.proxy.address);
+      tokenUser = await this.token.balanceOf.call(user);
+    });
+
+    describe('Exact input', function() {
+      it('normal', async function() {
+        const value = ether('1');
+        const to = this.hOneInchV2.address;
+        const slippage = 3;
+
+        const swapReq = queryString.stringifyUrl({
+          url: 'https://api.1inch.exchange/v2.0/swap',
+          query: {
+            fromTokenAddress: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+            toTokenAddress: tokenAddress,
+            amount: value,
+            slippage: slippage,
+            disableEstimate: true,
+            fromAddress: this.proxy.address,
+          },
+        });
+
+        const swapResponse = await fetch(swapReq);
+        const swapData = await swapResponse.json();
+        const txData = swapData.tx.data;
+        const txValue = swapData.tx.value;
+        const data = abi.simpleEncode(
+          'swap(uint256,address,address,uint256,bytes)',
+          txValue,
+          swapData.fromToken.address,
+          swapData.toToken.address,
+          swapData.fromTokenAmount,
+          util.toBuffer(txData)
+        );
+        const quote = swapData.toTokenAmount;
+        const receipt = await this.proxy.execMock(to, data, {
+          from: user,
+          value: value,
+        });
+
+        const tokenUserEnd = await this.token.balanceOf.call(user);
+        const handlerReturn = utils.toBN(
+          getHandlerReturn(receipt, ['uint256'])[0]
+        );
+        expect(handlerReturn).to.be.bignumber.eq(tokenUserEnd.sub(tokenUser));
+
+        expect(tokenUserEnd).to.be.bignumber.gte(
+          // sub 1 more percent to tolerate the slippage calculation difference with 1inch
+          tokenUser.add(mulPercent(quote, 100 - slippage - 1))
+        );
+        expect(
+          await this.token.balanceOf.call(this.proxy.address)
+        ).to.be.bignumber.eq(ether('0'));
+        expect(await balanceProxy.get()).to.be.bignumber.eq(ether('0'));
+        expect(await balanceUser.delta()).to.be.bignumber.eq(
+          ether('0')
+            .sub(value)
+            .sub(new BN(receipt.receipt.gasUsed))
+        );
+
+        profileGas(receipt);
+      });
+
+      it('msg.value greater than input ether amount', async function() {
+        const value = ether('1');
+        const to = this.hOneInchV2.address;
+        const slippage = 3;
+
+        const swapReq = queryString.stringifyUrl({
+          url: 'https://api.1inch.exchange/v2.0/swap',
+          query: {
+            fromTokenAddress: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+            toTokenAddress: tokenAddress,
+            amount: value,
+            slippage: slippage,
+            disableEstimate: true,
+            fromAddress: this.proxy.address,
+          },
+        });
+
+        const swapResponse = await fetch(swapReq);
+        const swapData = await swapResponse.json();
+        const txData = swapData.tx.data;
+        const txValue = swapData.tx.value;
+        const data = abi.simpleEncode(
+          'swap(uint256,address,address,uint256,bytes)',
+          txValue,
+          swapData.fromToken.address,
+          swapData.toToken.address,
+          swapData.fromTokenAmount,
+          util.toBuffer(txData)
+        );
+        const quote = swapData.toTokenAmount;
+        const receipt = await this.proxy.execMock(to, data, {
+          from: user,
+          value: value.add(ether('1')),
+        });
+
+        const tokenUserEnd = await this.token.balanceOf.call(user);
+        const handlerReturn = utils.toBN(
+          getHandlerReturn(receipt, ['uint256'])[0]
+        );
+        expect(handlerReturn).to.be.bignumber.eq(tokenUserEnd.sub(tokenUser));
+
+        expect(tokenUserEnd).to.be.bignumber.gte(
+          // sub 1 more percent to tolerate the slippage calculation difference with 1inch
+          tokenUser.add(mulPercent(quote, 100 - slippage - 1))
+        );
+        expect(
+          await this.token.balanceOf.call(this.proxy.address)
+        ).to.be.bignumber.eq(ether('0'));
+        expect(await balanceProxy.get()).to.be.bignumber.eq(ether('0'));
+        expect(await balanceUser.delta()).to.be.bignumber.eq(
+          ether('0')
+            .sub(value)
+            .sub(new BN(receipt.receipt.gasUsed))
+        );
+
+        profileGas(receipt);
+      });
+    });
+  });
+
+  describe('Token to Ether', function() {
+    const tokenAddress = DAI_TOKEN;
+    const tokenSymbol = DAI_SYMBOL;
+    const providerAddress = DAI_PROVIDER;
+
+    let balanceUser;
+    let balanceProxy;
+    let tokenUser;
+
+    before(async function() {
+      this.token = await IToken.at(tokenAddress);
+    });
+
+    beforeEach(async function() {
+      balanceUser = await tracker(user);
+      balanceProxy = await tracker(this.proxy.address);
+      tokenUser = await this.token.balanceOf.call(user);
+    });
+
+    describe('Exact input', function() {
+      it('normal', async function() {
+        const value = ether('50');
+        const to = this.hOneInchV2.address;
+        const slippage = 3;
+
+        const swapReq = queryString.stringifyUrl({
+          url: 'https://api.1inch.exchange/v2.0/swap',
+          query: {
+            fromTokenAddress: tokenAddress,
+            toTokenAddress: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+            amount: value,
+            slippage: slippage,
+            disableEstimate: true,
+            fromAddress: this.proxy.address,
+          },
+        });
+
+        await this.token.transfer(this.proxy.address, value, {
+          from: providerAddress,
+        });
+        await this.proxy.updateTokenMock(this.token.address);
+
+        const swapResponse = await fetch(swapReq);
+        const swapData = await swapResponse.json();
+        const txData = swapData.tx.data;
+        const txValue = swapData.tx.value;
+        const data = abi.simpleEncode(
+          'swap(uint256,address,address,uint256,bytes)',
+          txValue,
+          swapData.fromToken.address,
+          swapData.toToken.address,
+          swapData.fromTokenAmount,
+          util.toBuffer(txData)
+        );
+        const quote = swapData.toTokenAmount;
+        const receipt = await this.proxy.execMock(to, data, {
+          from: user,
+          value: ether('0.1'),
+        });
+
+        const balanceUserDelta = await balanceUser.delta();
+
+        const handlerReturn = utils.toBN(
+          getHandlerReturn(receipt, ['uint256'])[0]
+        );
+        expect(handlerReturn).to.be.bignumber.eq(
+          balanceUserDelta.add(new BN(receipt.receipt.gasUsed))
+        );
+
+        expect(await this.token.balanceOf.call(user)).to.be.bignumber.eq(
+          tokenUser
+        );
+        expect(
+          await this.token.balanceOf.call(this.proxy.address)
+        ).to.be.bignumber.eq(ether('0'));
+        expect(await balanceProxy.get()).to.be.bignumber.eq(ether('0'));
+        expect(balanceUserDelta).to.be.bignumber.gte(
+          ether('0')
+            // sub 1 more percent to tolerate the slippage calculation difference with 1inch
+            .add(mulPercent(quote, 100 - slippage - 1))
+            .sub(new BN(receipt.receipt.gasUsed))
+        );
+
+        profileGas(receipt);
+      });
+    });
+  });
+
+  describe('Token to Token', function() {
+    const token0Address = DAI_TOKEN;
+    const token0Symbol = DAI_SYMBOL;
+    const token1Address = USDC_TOKEN;
+    const token1Symbol = USDC_SYMBOL;
+    const providerAddress = DAI_PROVIDER;
+
+    let token0User;
+    let token1User;
+
+    before(async function() {
+      this.token0 = await IToken.at(token0Address);
+      this.token1 = await IToken.at(token1Address);
+    });
+
+    beforeEach(async function() {
+      token0User = await this.token0.balanceOf.call(user);
+      token1User = await this.token1.balanceOf.call(user);
+    });
+
+    describe('Exact input', function() {
+      it('normal', async function() {
+        const value = ether('50');
+        const to = this.hOneInchV2.address;
+        const slippage = 3;
+
+        const swapReq = queryString.stringifyUrl({
+          url: 'https://api.1inch.exchange/v2.0/swap',
+          query: {
+            fromTokenAddress: token0Address,
+            toTokenAddress: token1Address,
+            amount: value,
+            slippage: slippage,
+            disableEstimate: true,
+            fromAddress: this.proxy.address,
+          },
+        });
+
+        await this.token0.transfer(this.proxy.address, value, {
+          from: providerAddress,
+        });
+        await this.proxy.updateTokenMock(this.token0.address);
+
+        const swapResponse = await fetch(swapReq);
+        const swapData = await swapResponse.json();
+        const txData = swapData.tx.data;
+        const txValue = swapData.tx.value;
+        const data = abi.simpleEncode(
+          'swap(uint256,address,address,uint256,bytes)',
+          txValue,
+          swapData.fromToken.address,
+          swapData.toToken.address,
+          swapData.fromTokenAmount,
+          util.toBuffer(txData)
+        );
+        const quote = swapData.toTokenAmount;
+        const receipt = await this.proxy.execMock(to, data, {
+          from: user,
+          value: ether('0.1'),
+        });
+
+        const token1UserEnd = await this.token1.balanceOf.call(user);
+        const handlerReturn = utils.toBN(
+          getHandlerReturn(receipt, ['uint256'])[0]
+        );
+        expect(handlerReturn).to.be.bignumber.eq(token1UserEnd.sub(token1User));
+
+        expect(await this.token0.balanceOf.call(user)).to.be.bignumber.eq(
+          token0User
+        );
+        expect(
+          await this.token0.balanceOf.call(this.proxy.address)
+        ).to.be.bignumber.eq(ether('0'));
+        expect(
+          await this.token1.balanceOf.call(this.proxy.address)
+        ).to.be.bignumber.eq(ether('0'));
+        expect(await this.token1.balanceOf.call(user)).to.be.bignumber.gte(
+          // sub 1 more percent to tolerate the slippage calculation difference with 1inch
+          token1User.add(mulPercent(quote, 100 - slippage - 1))
+        );
+
+        profileGas(receipt);
+      });
+    });
+  });
+});
