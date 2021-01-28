6d07eb1a0716bcb2f750474116c6feea69e9c18a
benhuang <ben@dinngo.co>
2021-01-28 17:42:53 +0800

    Bump version to 1.2.0

--- migrations/15_hcurve.js
+++ migrations/15_hcurve.js
@@ -7,6 +7,9 @@ module.exports = async function(deployer) {
     return;
   }
   await deployer.deploy(Handler);
-  const registry = await Registry.deployed();
+  //const registry = await Registry.deployed();
+  const registry = await Registry.at(
+    '0x1B742498dA0Aa60aE55e7a8673105635DBD7C64B'
+  );
   await registry.register(Handler.address, utils.asciiToHex('HCurve'));
 };
