65b36cf990a21af718447ea99fd3ffc32d7d8ce8
Robin Pan <robin@dinngo.co>
2021-05-06 16:19:55 +0800

    Prettify

--- contracts/lib/LibFeeStorage.sol
+++ contracts/lib/LibFeeStorage.sol
@@ -5,7 +5,7 @@ import "../Storage.sol";
 
 library LibFeeStorage {
     using LibCache for mapping(bytes32 => bytes32);
-    
+
     // keccak256 hash of "fee.rate"
     // prettier-ignore
     bytes32 public constant FEE_RATE_KEY = 0x515323fd14ae1d8e2508b9830a3e80e1a884361823ecf1c0f4d3f345ad226225;
@@ -14,22 +14,32 @@ library LibFeeStorage {
     // prettier-ignore
     bytes32 public constant FEE_COLLECTOR_KEY = 0x0efe2dc3698afe8504a41468034ba33d504aa66b651e1d37f230a3ca6ee9dc72;
 
-    function _setFeeRate(mapping(bytes32 => bytes32) storage _cache, uint256 _feeRate) internal {
+    function _setFeeRate(
+        mapping(bytes32 => bytes32) storage _cache,
+        uint256 _feeRate
+    ) internal {
         require(_getFeeRate(_cache) == 0, "Fee rate not zero");
         _cache.setUint256(FEE_RATE_KEY, _feeRate);
     }
 
-    function _resetFeeRate(mapping(bytes32 => bytes32) storage _cache) internal {
+    function _resetFeeRate(mapping(bytes32 => bytes32) storage _cache)
+        internal
+    {
         _cache.setUint256(FEE_RATE_KEY, 0);
     }
 
-    function _getFeeRate(mapping(bytes32 => bytes32) storage _cache) internal view returns (uint256) {
+    function _getFeeRate(mapping(bytes32 => bytes32) storage _cache)
+        internal
+        view
+        returns (uint256)
+    {
         return _cache.getUint256(FEE_RATE_KEY);
     }
 
-    function _setFeeCollector(mapping(bytes32 => bytes32) storage _cache, address _collector)
-        internal
-    {
+    function _setFeeCollector(
+        mapping(bytes32 => bytes32) storage _cache,
+        address _collector
+    ) internal {
         require(
             _getFeeCollector(_cache) == address(0),
             "Fee collector is initialized"
@@ -37,11 +47,17 @@ library LibFeeStorage {
         _cache.setAddress(FEE_COLLECTOR_KEY, _collector);
     }
 
-    function _resetFeeCollector(mapping(bytes32 => bytes32) storage _cache) internal {
+    function _resetFeeCollector(mapping(bytes32 => bytes32) storage _cache)
+        internal
+    {
         _cache.setAddress(FEE_COLLECTOR_KEY, address(0));
     }
 
-    function _getFeeCollector(mapping(bytes32 => bytes32) storage _cache) internal view returns (address) {
+    function _getFeeCollector(mapping(bytes32 => bytes32) storage _cache)
+        internal
+        view
+        returns (address)
+    {
         return _cache.getAddress(FEE_COLLECTOR_KEY);
     }
 }
