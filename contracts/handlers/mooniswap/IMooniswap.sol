87b09f57b903559bbd061da00caadc362afc9d0a
benhuang <ben@dinngo.co>
2021-05-24 14:37:49 +0800

    Archive Uniswap and Mooniswap handlers

--- contracts/handlers/mooniswap/IMooniswap.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity >=0.6.0;
-
-interface IMooniswap {
-    function deposit(uint256[] calldata, uint256[] calldata) external payable returns(uint256);
-    function withdraw(uint256, uint256[] calldata) external;
-    function getTokens() external view returns(address[] memory);
-}
