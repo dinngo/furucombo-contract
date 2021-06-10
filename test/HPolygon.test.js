3116ce47805793a71f178c245183af8c049572b1
Robin Pan <robin@dinngo.co>
2021-06-10 11:21:44 +0800

    Add test for HPolygon

---
+++ test/HPolygon.test.js
@@ -0,0 +1,164 @@
+const {
+  balance,
+  BN,
+  constants,
+  ether,
+  expectEvent,
+  expectRevert,
+  time,
+} = require('@openzeppelin/test-helpers');
+const { tracker } = balance;
+const { latest } = time;
+const abi = require('ethereumjs-abi');
+const utils = web3.utils;
+
+const { expect } = require('chai');
+
+const { DAI_TOKEN, DAI_PROVIDER, MATIC_TOKEN, POLYGON_POS_PREDICATE_ERC20, POLYGON_POS_PREDICATE_ETH, ETH_TOKEN, POLYGON_PLASMA_DEPOSIT_MANAGER, MATIC_PROVIDER } = require('./utils/constants');
+const { evmRevert, evmSnapshot, profileGas, getCallData } = require('./utils/utils');
+
+const HPolygon = artifacts.require('HPolygon');
+const Registry = artifacts.require('Registry');
+const Proxy = artifacts.require('ProxyMock');
+const IToken = artifacts.require('IERC20');
+
+contract('Polygon Token Bridge', function([_, user]) {
+  const tokenAddress = DAI_TOKEN;
+  const providerAddress = DAI_PROVIDER;
+  let id;
+
+  before(async function() {
+    this.registry = await Registry.new();
+    this.proxy = await Proxy.new(this.registry.address);
+    this.hPolygon = await HPolygon.new();
+    await this.registry.register(this.hPolygon.address, utils.asciiToHex('Polygon'));
+    this.token = await IToken.at(tokenAddress);
+    this.matic = await IToken.at(MATIC_TOKEN);
+  });
+
+  beforeEach(async function() {
+    id = await evmSnapshot();
+  });
+
+  afterEach(async function() {
+    await evmRevert(id);
+  });
+
+  describe('PoS Bridge', function() {
+    beforeEach(async function() {
+      tokenUserAmount = await this.token.balanceOf.call(user);
+      balanceProxy = await tracker(this.proxy.address);
+      balanceUser = await tracker(user);
+      // balance of bridge
+      tokenBridgeAmount = await this.token.balanceOf.call(POLYGON_POS_PREDICATE_ERC20);
+      balanceBridge = await tracker(POLYGON_POS_PREDICATE_ETH);
+    });
+
+    it('ether', async function() {
+      // Prepare handler data
+      const value = ether('10');
+      const to = this.hPolygon.address;
+      const data = getCallData(HPolygon, 'depositEther', [
+        value
+      ]);
+
+      // Execute
+      const receipt = await this.proxy.execMock(to, data, {
+        from: user,
+        value: value,
+      });
+
+      // Verify balance
+      expect(await balanceBridge.delta()).to.be.bignumber.eq(value);
+      expect(await balanceProxy.get()).to.be.zero;
+      expect(await balanceUser.delta()).to.be.bignumber.eq(
+        ether('0')
+          .sub(value)
+          .sub(new BN(receipt.receipt.gasUsed))
+      );
+      profileGas(receipt);
+    });
+
+    it('token', async function() {
+      // Prepare handler data
+      const token = this.token.address;
+      const value = ether('100');
+      const to = this.hPolygon.address;
+      const data = getCallData(HPolygon, 'depositERC20', [
+        token,
+        value
+      ]);
+
+      // Send tokens to proxy
+      await this.token.transfer(this.proxy.address, value, {
+        from: providerAddress,
+      });
+      await this.proxy.updateTokenMock(this.token.address);
+
+      // Execute
+      const receipt = await this.proxy.execMock(to, data, {
+        from: user,
+        value: ether('0.1'),
+      });
+
+      // Verify Bridge balance
+      expect(await this.token.balanceOf.call(POLYGON_POS_PREDICATE_ERC20)).to.be.bignumber.eq(tokenBridgeAmount.add(value));
+      // Verify Proxy balance
+      expect(await this.token.balanceOf.call(this.proxy.address)).to.be.zero;
+      expect(await balanceProxy.get()).to.be.zero;
+      // Verify User balance
+      expect(await this.token.balanceOf.call(user)).to.be.zero;
+      expect(await balanceUser.delta()).to.be.bignumber.eq(
+        ether('0')
+          .sub(new BN(receipt.receipt.gasUsed))
+      );
+      profileGas(receipt);
+    });
+  });
+
+  describe('Plasma Bridge', function() {
+    beforeEach(async function() {
+      maticUserAmount = await this.matic.balanceOf.call(user);
+      balanceProxy = await tracker(this.proxy.address);
+      balanceUser = await tracker(user);
+      // balance of bridge
+      maticBridgeAmount = await this.matic.balanceOf.call(POLYGON_PLASMA_DEPOSIT_MANAGER);
+    });
+
+    it('MATIC', async function() {
+      // Prepare handler data
+      const token = this.matic.address;
+      const value = ether('100');
+      const to = this.hPolygon.address;
+      const data = getCallData(HPolygon, 'depositERC20', [
+        token,
+        value
+      ]);
+
+      // Send tokens to proxy
+      await this.matic.transfer(this.proxy.address, value, {
+        from: MATIC_PROVIDER,
+      });
+      await this.proxy.updateTokenMock(this.matic.address);
+
+      // Execute
+      const receipt = await this.proxy.execMock(to, data, {
+        from: user,
+        value: ether('0.1'),
+      });
+
+      // Verify Bridge balance
+      expect(await this.matic.balanceOf.call(POLYGON_PLASMA_DEPOSIT_MANAGER)).to.be.bignumber.eq(maticBridgeAmount.add(value));
+      // Verify Proxy balance
+      expect(await this.matic.balanceOf.call(this.proxy.address)).to.be.zero;
+      expect(await balanceProxy.get()).to.be.zero;
+      // Verify User balance
+      expect(await this.matic.balanceOf.call(user)).to.be.zero;
+      expect(await balanceUser.delta()).to.be.bignumber.eq(
+        ether('0')
+          .sub(new BN(receipt.receipt.gasUsed))
+      );
+      profileGas(receipt);
+    });
+  });
+});
