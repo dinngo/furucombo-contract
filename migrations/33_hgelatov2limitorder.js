303eaa48cccf29840a7b9aa5818fe82f32a89c8b
Albert Lin <albert@dinngo.co>
2021-06-18 17:21:15 +0800

    refactor: fix gelato migration file

--- migrations/33_hgelatov2limitorder.js
+++ migrations/33_hgelatov2limitorder.js
@@ -2,11 +2,17 @@ const Registry = artifacts.require('Registry');
 const Handler = artifacts.require('HGelatoV2LimitOrder');
 const utils = web3.utils;
 
+const {
+  GELATOV2_PINE,
+  GELATOV2_LIMIT_ORDER_MODULE,
+} = require('../test/utils/constants');
+
 module.exports = async function(deployer) {
   if (deployer.network === 'development') {
     return;
   }
-  await deployer.deploy(Handler);
+
+  await deployer.deploy(Handler, GELATOV2_PINE, GELATOV2_LIMIT_ORDER_MODULE);
   const registry = await Registry.deployed();
   await registry.register(
     Handler.address,
