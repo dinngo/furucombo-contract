e4506713a40cfa9121b6634bc48dac60903bcc88
Albert Lin <albert@dinngo.co>
2021-06-21 02:37:08 +0800

    fix gelatov2 handler migration file

--- migrations/33_hgelatov2limitorder.js
+++ migrations/33_hgelatov2limitorder.js
@@ -2,10 +2,9 @@ const Registry = artifacts.require('Registry');
 const Handler = artifacts.require('HGelatoV2LimitOrder');
 const utils = web3.utils;
 
-const {
-  GELATOV2_PINE,
-  GELATOV2_LIMIT_ORDER_MODULE,
-} = require('../test/utils/constants');
+const GELATOV2_PINE = '0x36049D479A97CdE1fC6E2a5D2caE30B666Ebf92B';
+const GELATOV2_LIMIT_ORDER_MODULE =
+  '0x037fc8e71445910e1E0bBb2a0896d5e9A7485318';
 
 module.exports = async function(deployer) {
   if (deployer.network === 'development') {
