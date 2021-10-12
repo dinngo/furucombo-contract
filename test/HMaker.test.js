f9d7b52e41a0eb1e7d033ea46b08a26be48e52ef
Mussina Lin <mussina@dinngo.co>
2021-10-12 18:01:05 +0800

    feat: temp comment some Maker test due to  Vaults Ceiling = 0

--- test/HMaker.test.js
+++ test/HMaker.test.js
@@ -354,7 +354,7 @@ contract('Maker', function([_, user1, user2, someone]) {
 
     describe('Lock Token', function() {
       describe('Draw Dai', function() {
-        it('User does not has proxy', async function() {
+        it.skip('User does not has proxy', async function() {
           const daiUser = await this.dai.balanceOf.call(someone);
 
           expect(await this.dsRegistry.proxies.call(this.proxy.address)).not.eq(
@@ -408,7 +408,7 @@ contract('Maker', function([_, user1, user2, someone]) {
           profileGas(receipt);
         });
 
-        it('User has proxy', async function() {
+        it.skip('User has proxy', async function() {
           const to = this.hMaker.address;
           const ilkToken = utils.padRight(utils.asciiToHex('LINK-A'), 64);
           const [
@@ -932,7 +932,7 @@ contract('Maker', function([_, user1, user2, someone]) {
         [ilk, debt, lock] = await getCdpInfo(cdp);
       });
 
-      it('normal', async function() {
+      it.skip('normal', async function() {
         await approveCdp(cdp, user1, this.dsProxy.address);
         const to = this.hMaker.address;
         const [
@@ -1101,7 +1101,7 @@ contract('Maker', function([_, user1, user2, someone]) {
       });
     });
 
-    describe('pay back to gem cdp', function() {
+    describe.skip('pay back to gem cdp', function() {
       let cdp;
       let ilk;
       let debt;
