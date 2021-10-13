d069da533be1c651b7d84ee6cc63e4c15a7c9b95
Mussina Lin <mussina@dinngo.co>
2021-10-13 20:03:28 +0800

    refactor: delete unused comment in deploy script

--- deploy/06_deploy_hkybernetwork.js
+++ deploy/06_deploy_hkybernetwork.js
@@ -19,10 +19,6 @@ module.exports = async (hre) => {
     hKyberNetwork.address,
         utils.hexlify(utils.formatBytes32String('Kyber Network'))
       );
-
-  // console.log('addr:' + hKyberNetwork.address);
-  // var a = await registry.isValidHandler.call(hKyberNetwork.address);
-  // console.log('isValid:' + a);
 };
 
 
--- deploy/08_deploy_hmaker.js
+++ deploy/08_deploy_hmaker.js
@@ -23,42 +23,6 @@ module.exports = async (hre) => {
   console.log('dsproxy: ' + (await dsRegistry.proxies(proxy.address)));
 };
 
-// const { ethers } = require("hardhat");
-// const utils = ethers.utils;
-// const MAKER_PROXY_REGISTRY = '0x4678f0a6958e4D2Bc4F1BAF7Bc52E8F3564f3fE4';
 
-// module.exports = async (hre) => {
-//   const { deployments } = hre;
-//   const { deploy } = deployments;
-//   const { deployer } = await hre.getNamedAccounts();
-
-//   await deploy('HMaker', {
-//     from: deployer,
-//     args: [],
-//     log: true,
-//   });
-
-  // console.log('dsRegistry');
-  // const DSRegistry = await ethers.getContractAt('IDSProxyRegistry', MAKER_PROXY_REGISTRY, deployer);
-  // console.log('succ1');
-  // console.log(DSRegistry);
-  // const dsRegistry = await DSRegistry.connect(MAKER_PROXY_REGISTRY);
-  // console.log('succ2');
-  // console.log(dsRegistry);
-
-//   const registry = await hre.ethers.getContract('Registry', deployer);
-//   const proxy = await hre.ethers.getContract('Proxy', deployer);
-//   const hMaker = await hre.ethers.getContract('HMaker', deployer);
-
-//   await registry.register(
-//     hMaker.address,
-//         utils.hexlify(utils.formatBytes32String('HMaker'))
-//       );
-      
-//   await DSRegistry.build();
-//   console.log('dsproxy: ' + (await dsRegistry.proxies.call(proxy.address)));
-// };
-
-
-// module.exports.tags = ['HMaker'];
-// module.exports.dependencies = ["Registry"];
+module.exports.tags = ['HMaker'];
+module.exports.dependencies = ["Registry"];
