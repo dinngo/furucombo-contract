efcec7ce8a97582bc25c530970b186229a25c5b0
Mussina Lin <mussina@dinngo.co>
2021-10-13 19:01:04 +0800

    feat: delete truffle.config.  delete ./migrations

--- migrations/1_initial_migration.js
+++
@@ -1,8 +0,0 @@
-const Migrations = artifacts.require('Migrations');
-
-module.exports = function(deployer) {
-  if (deployer.network === 'development') {
-    return;
-  }
-  deployer.deploy(Migrations);
-};
