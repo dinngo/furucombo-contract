70e115bbeb65b1051307e7f49577f3c8e75be691
Mussina Lin <mussina@dinngo.co>
2022-02-09 14:27:01 +0800

    feat: use ethers.getSigners() instead ethers.wallet.

--- deploy/30_deploy_hparaswapv2.js
+++ deploy/30_deploy_hparaswapv2.js
@@ -3,8 +3,7 @@ const utils = ethers.utils;
 // beta parameter
 const registryOwner = '0xA7248F4B85FB6261c314d08e7938285d1d86cd61';
 const registryAddress = '0x84Faf5b559f58d651E51300C5a7D078cb964E691';
-const fakeKey =
-  'd74d952106fcdc7fe598eea01a3e9f5a081d928cea7869e9921e69abc5a7dd44';
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -38,7 +37,7 @@ async function betaDeployment(hParaSwapV5) {
   console.log('beta deployment...');
 
   const provider = ethers.provider;
-  const signer = new ethers.Wallet(fakeKey, provider);
+  const [signer] = await ethers.getSigners();
 
   // register to Registry
   const iface = new utils.Interface([
