06b425c6e200aae6ba56562127eb281efd158ee3
Jeff Huang <jeff@dinngo.co>
2022-10-25 13:17:56 +0800

    restore deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,7 +19,7 @@ hardhat_running() {
 
 start_hardhat() {
     
-    npx hardhat node --fork $RPC_URL &
+    npx hardhat node --fork $ETH_MAINNET_NODE &
     echo "deployment script will be executed" 
 
     hardhat_pid=$!
