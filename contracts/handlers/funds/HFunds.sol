bacd075f03bfcb1ace8e5d18b9d580ed7fed45cf
Javan Chen <javan@dinngo.co>
2023-02-17 15:12:22 +0800

    fix: uncheck ++index in for loop and update prettier version

--- contracts/handlers/funds/HFunds.sol
+++ contracts/handlers/funds/HFunds.sol
@@ -16,37 +16,36 @@ contract HFunds is HandlerBase {
         return "HFunds";
     }
 
-    function updateTokens(address[] calldata tokens)
-        external
-        payable
-        returns (uint256[] memory)
-    {
+    function updateTokens(
+        address[] calldata tokens
+    ) external payable returns (uint256[] memory) {
         uint256[] memory balances = new uint256[](tokens.length);
-        for (uint256 i = 0; i < tokens.length; i++) {
+        for (uint256 i = 0; i < tokens.length; ) {
             address token = tokens[i];
             if (token != address(0) && token != NATIVE_TOKEN_ADDRESS) {
                 // Update involved token
                 _updateToken(token);
             }
             balances[i] = _getBalance(token, type(uint256).max);
+            unchecked {
+                ++i;
+            }
         }
         return balances;
     }
 
-    function inject(address[] calldata tokens, uint256[] calldata amounts)
-        external
-        payable
-        returns (uint256[] memory)
-    {
+    function inject(
+        address[] calldata tokens,
+        uint256[] calldata amounts
+    ) external payable returns (uint256[] memory) {
         return _inject(tokens, amounts);
     }
 
     // Same as inject() and just to make another interface for different use case
-    function addFunds(address[] calldata tokens, uint256[] calldata amounts)
-        external
-        payable
-        returns (uint256[] memory)
-    {
+    function addFunds(
+        address[] calldata tokens,
+        uint256[] calldata amounts
+    ) external payable returns (uint256[] memory) {
         return _inject(tokens, amounts);
     }
 
@@ -55,7 +54,7 @@ contract HFunds is HandlerBase {
         uint256[] calldata amounts,
         address payable receiver
     ) external payable {
-        for (uint256 i = 0; i < tokens.length; i++) {
+        for (uint256 i = 0; i < tokens.length; ) {
             uint256 amount = _getBalance(tokens[i], amounts[i]);
             if (amount > 0) {
                 // ETH case
@@ -67,6 +66,9 @@ contract HFunds is HandlerBase {
                 } else {
                     IERC20(tokens[i]).safeTransfer(receiver, amount);
                 }
+            }
+            unchecked {
+                ++i;
             }
         }
     }
@@ -82,13 +84,16 @@ contract HFunds is HandlerBase {
             "amount and receiver does not match"
         );
         if (_isNotNativeToken(token)) {
-            for (uint256 i = 0; i < amounts.length; i++) {
+            for (uint256 i = 0; i < amounts.length; ) {
                 if (amounts[i] > 0) {
                     IERC20(token).safeTransfer(receivers[i], amounts[i]);
                 }
+                unchecked {
+                    ++i;
+                }
             }
         } else {
-            for (uint256 i = 0; i < amounts.length; i++) {
+            for (uint256 i = 0; i < amounts.length; ) {
                 if (amounts[i] > 0) {
                     (bool success, ) = receivers[i].call{value: amounts[i]}("");
                     _requireMsg(
@@ -96,6 +101,9 @@ contract HFunds is HandlerBase {
                         "sendTokenToAddresses",
                         "failed to send Ether"
                     );
+                }
+                unchecked {
+                    ++i;
                 }
             }
         }
@@ -140,36 +148,35 @@ contract HFunds is HandlerBase {
             "token and amount do not match"
         );
 
-        for (uint256 i = 0; i < tokens.length; i++) {
+        for (uint256 i = 0; i < tokens.length; ) {
             if (tokens[i] == address(0)) {
                 if (address(this).balance < amounts[i]) {
-                    string memory errMsg =
-                        string(
-                            abi.encodePacked(
-                                "error: ",
-                                _uint2String(i),
-                                "_",
-                                _uint2String(address(this).balance)
-                            )
-                        );
+                    string memory errMsg = string(
+                        abi.encodePacked(
+                            "error: ",
+                            _uint2String(i),
+                            "_",
+                            _uint2String(address(this).balance)
+                        )
+                    );
                     _revertMsg("checkSlippage", errMsg);
                 }
             } else if (
                 IERC20(tokens[i]).balanceOf(address(this)) < amounts[i]
             ) {
-                string memory errMsg =
-                    string(
-                        abi.encodePacked(
-                            "error: ",
-                            _uint2String(i),
-                            "_",
-                            _uint2String(
-                                IERC20(tokens[i]).balanceOf(address(this))
-                            )
-                        )
-                    );
+                string memory errMsg = string(
+                    abi.encodePacked(
+                        "error: ",
+                        _uint2String(i),
+                        "_",
+                        _uint2String(IERC20(tokens[i]).balanceOf(address(this)))
+                    )
+                );
 
                 _revertMsg("checkSlippage", errMsg);
+            }
+            unchecked {
+                ++i;
             }
         }
     }
@@ -178,10 +185,10 @@ contract HFunds is HandlerBase {
         return _getBalance(token, type(uint256).max);
     }
 
-    function _inject(address[] calldata tokens, uint256[] calldata amounts)
-        internal
-        returns (uint256[] memory)
-    {
+    function _inject(
+        address[] calldata tokens,
+        uint256[] calldata amounts
+    ) internal returns (uint256[] memory) {
         _requireMsg(
             tokens.length == amounts.length,
             "inject",
@@ -192,7 +199,7 @@ contract HFunds is HandlerBase {
         address collector = cache._getFeeCollector();
         uint256[] memory amountsInProxy = new uint256[](amounts.length);
 
-        for (uint256 i = 0; i < tokens.length; i++) {
+        for (uint256 i = 0; i < tokens.length; ) {
             IERC20(tokens[i]).safeTransferFrom(
                 sender,
                 address(this),
@@ -209,15 +216,18 @@ contract HFunds is HandlerBase {
 
             // Update involved token
             _updateToken(tokens[i]);
+
+            unchecked {
+                ++i;
+            }
         }
         return amountsInProxy;
     }
 
-    function _calFee(uint256 amount, uint256 feeRate)
-        internal
-        pure
-        returns (uint256)
-    {
+    function _calFee(
+        uint256 amount,
+        uint256 feeRate
+    ) internal pure returns (uint256) {
         return (amount * feeRate) / PERCENTAGE_BASE;
     }
 }
