ef6d91659b39e5e2e2a2db7fc4149a37989b50d7
AlbertLin <albert@dinngo.co>
2023-03-03 15:13:00 +0800

    fix: fix geist handler deploy file

--- deploy/handlers/HGeistPostSetup.js
+++ deploy/handlers/HGeistPostSetup.js
@@ -1,9 +1,11 @@
-const { get, registerHandler } = require('../utils/deploy.js');
+const { get, registerHandler, registerCaller } = require('../utils/deploy.js');
+const { GEIST_LENDING_POOL } = require('../utils/addresses.js');
 
 module.exports = async () => {
   const registry = await get('Registry');
   const handler = await get('HGeist');
   await registerHandler(registry, handler);
+  await registerCaller(registry, GEIST_LENDING_POOL, handler);
 };
 
 module.exports.tags = ['HGeistPostSetup'];
