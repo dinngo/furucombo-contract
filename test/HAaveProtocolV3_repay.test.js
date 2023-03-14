19a692bcd571abfb1fdfae8ed09cae48dd71feeb
AlbertLin <albert@dinngo.co>
2023-03-14 20:55:10 +0800

    test: Skip AAVEV3 test cases on AVAX because AAVEV3 on AVAX is currently frozen

--- test/HAaveProtocolV3_repay.test.js
+++ test/HAaveProtocolV3_repay.test.js
@@ -4,8 +4,8 @@ if (
   chainId == 1 ||
   chainId == 10 ||
   chainId == 137 ||
-  chainId == 42161 ||
-  chainId == 43114
+  chainId == 42161
+  // chainId == 43114 TODO: skip becase AAVEV3 of AVAX is FROZEN
 ) {
   // This test supports to run on these chains.
 } else {
